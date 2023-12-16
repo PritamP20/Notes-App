836864ea499150f868bc09f83d2222350a5908bc
PritamP20 <pripritam7@gmail.com>
2023-12-16 21:24:40 +0530

    added cors permission

--- src/components/CSE/Preview.jsx
+++ src/components/CSE/Preview.jsx
@@ -1,10 +1,41 @@
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import { getStorage, ref,getDownloadURL } from "firebase/storage";
+
 
 const Preview = (props) => {
 
     const location = useLocation();
 
+    // gsutil cors set cors.json gs://fir-app-5920f.appspot.com/files
+
+    const storage = getStorage();
+    getDownloadURL(ref(storage, 'files/Unit 3_Mechanics.pdf'))
+      .then((url) => {
+        // `url` is the download URL for 'images/stars.jpg'
+    
+        console.log("This is the url of mechanics",url)
+
+        // This can be downloaded directly:
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = (event) => {
+          const blob = xhr.response;
+        };
+        xhr.open('GET', url);
+        xhr.send();
+    
+        // Or inserted into an <img> element
+        // const img = document.getElementById('myimg');
+        // img.setAttribute('src', url);
+      })
+      .catch((error) => {
+        // Handle any errors
+      });
+
+      useEffect(()=>{
+        getDownloadURL()
+      },[])
 
   return (
     <>
