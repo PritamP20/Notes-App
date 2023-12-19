f4e6c696573cf22bf16a7e5d3aa12eb0d373b99f
MahalasaSP <mahalasahsh@gmail.com>
2023-12-19 12:53:52 +0530

    created a reference page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Contact from './components/Contact';
 import Upload from './components/Upload';
 import Chemistry from './components/CSE/Chemistry';
 import Preview from './components/CSE/Preview';
+import Reference from './components/Reference';
 
 // import MyCustomButton from './components/MyCustomButton';
 
@@ -36,6 +37,7 @@ function App() {
           <Route path="/CSE" element={<CSE />} />
           <Route path="/Sem_1" element={<Sem_1 />} />
           <Route path="/Physics" element={<Physics />} />
+          <Route path="/Reference" element={<Reference />} />
           {/* <Route path="/Mybutton" element={<MyCustomButton />} /> */}
           {/* <Route path="/upload" element={UploadButton} /> */}
           <Route path="/Chemistry" element={<Chemistry />} />
