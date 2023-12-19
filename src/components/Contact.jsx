f1ece8448fd0a7382fab9f2f54ae55a2483af8d0
PritamP20 <pripritam7@gmail.com>
2023-12-19 20:29:32 +0530

    feedback ui improvement

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export default class Contact extends Component {
 
   render() {
     return (
-      <div className="relative flex flex-col items-center justify-center h-screen">
+      <div className="relative flex flex-col pt-36 items-center h-screen">
         <div className="text-center mb-8">
           <h1 className="text-2xl font-extrabold text-gray-900 dark:text-Black md:text-5xl lg:text-3xl">
             Your Feedback matters
