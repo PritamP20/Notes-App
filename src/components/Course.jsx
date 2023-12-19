db9366db11238524e661b46f950cb400b0bac87c
PritamP20 <pripritam7@gmail.com>
2023-12-19 11:04:55 +0530

    course

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -29,22 +29,23 @@ const Course = () => {
       <button><strong>ELECTRICAL</strong></button>
       </button>
     </div>
-    <div className='flex justify-between pl-48 pr-52 pt-16 '>
+   
+    <div className='flex justify-between pl-20 pr-20 pt-16 '>
+    <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
+      <img className="h-20" src="/src/assets/books.png" alt="" />
+      <hr />
+      <button><strong>CSE</strong></button>
+      </button>
+    <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
+      <img className="h-20" src="/src/assets/books.png" alt="" />
+      <hr />
+      <button><strong>MECHANICAL</strong></button>
+      </button>
     <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
       <img className="h-20" src="/src/assets/books.png" alt="" />
       <hr />
       <button><strong>ELECTRICAL</strong></button>
-    </button>
-    <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-    </button>
-    <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-    </button>
+      </button>
     </div>
     <div className='flex justify-between pl-20 pr-20 pt-16 '>
     <button type="button" class="text-gray-900 text-xl bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg  px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
