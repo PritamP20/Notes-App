1cd6718e71bc8b16f3cc1020b935eb08e4347bad
BhuvanShridhar <bhuvanptr@gmail.com>
2023-12-21 08:30:54 +0530

    Just added Firebase for ref

---
+++ src/components/FeedbackFirebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIFvmD20ogOAa2hue7qFbp9r3W6118B_4",
+  authDomain: "fir-app-5920f.firebaseapp.com",
+  projectId: "fir-app-5920f",
+  storageBucket: "fir-app-5920f.appspot.com",
+  messagingSenderId: "380256160623",
+  appId: "1:380256160623:web:49d09bc7e2bc280863769a"
+};
+
+export const app = initializeApp(firebaseConfig)
+export const storage = getStorage(app)
+export const db = getFirestore(app)
