31b0eb81ad6eded0336483b71127ef4a57ba8313
PritamP20 <pripritam7@gmail.com>
2023-12-22 12:38:58 +0530

    ui

--- src/components/CSE/Physics.jsx
+++ src/components/CSE/Physics.jsx
@@ -12,7 +12,7 @@ const Physics = () => {
           {/* <Link to="/Preview" url="https://firebasestorage.googleapis.com/v0/b/fir-app-5920f.appspot.com/o/files%2FUnit%203_Mechanics.pdf?alt=media&token=5059f8b1-3f06-402d-8e2e-85dd4b345d51"> */}
           <div className='flex justify-center pt-8'>
             <button onClick={()=>{
-                navigate("/Preview",{state : "https://firebasestorage.googleapis.com/v0/b/fir-app-5920f.appspot.com/o/files%2FUnit%203_Mechanics.pdf?alt=media&token=5059f8b1-3f06-402d-8e2e-85dd4b345d51"})
+                navigate("/Preview",{state : "https://firebasestorage.googleapis.com/v0/b/fir-app-5920f.appspot.com/o/files%2FMaths%2FUnit%20-%20III%20-%20MCLA.pdf?alt=media&token=26b5b1ce-6f49-4d89-87d3-cae71048486c"})
             }}  type="button" className="shadow-xl flex-col justify-center items-end text-gray-900 bg-transparent border border-black focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700">
               <h1 className='text-xl'>Unit 1</h1>
               <p>Vector calculas</p>
